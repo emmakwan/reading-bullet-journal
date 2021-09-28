39cc22f525595733b049f1442b496a9e69e6a133
emmakwan <emhkwan@gmail.com>
2021-09-28 01:21:58 -0400

    Add DateTimePicker to AddEntryView

--- components/AddEntryView.tsx
+++ components/AddEntryView.tsx
@@ -1,8 +1,41 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useCallback, useState } from "react";
+import { StyleSheet, View, Pressable, Text } from "react-native";
+import DateTimePicker, {
+  Event as DatePickerEvent,
+} from "@react-native-community/datetimepicker";
 
 const AddEntryView = () => {
-  return <View style={styles.addEntryView}></View>;
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+
+  const handleOnChange: (event: DatePickerEvent, date?: Date) => void = (
+    event,
+    selectedDate
+  ) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+  };
+
+  const handleOnPress = useCallback(() => {
+    setShow((prevShow) => !prevShow);
+  }, []);
+
+  return (
+    <View style={styles.addEntryView}>
+      <Pressable onPress={handleOnPress}>
+        <Text>{date.toDateString()}</Text>
+      </Pressable>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode="date"
+          display="default"
+          onChange={handleOnChange}
+        />
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
